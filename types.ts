c5ca893ab7709672c266b37ae218c19712ca0d73
Amin Payandeh Far <amin.payandehfar94@gmail.com>
2022-11-13 21:37:31 +0330

    my first commit

---
+++ types.ts
@@ -0,0 +1,11 @@
+const today = new Date();
+today.getMonth();
+
+const person = {
+    age: 20
+};
+
+class Color {}
+const red = new Color();
+console.log(`person object has
+            age: ${person.age} property`)
