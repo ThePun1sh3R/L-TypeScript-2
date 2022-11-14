d5881e2959c73003b6bdc127aa17c5dbaf36fae0
Amin Payandeh Far <amin.payandehfar94@gmail.com>
2022-11-14 18:50:02 +0330

    new TS file for practice

---
+++ annotations/variables.ts
@@ -0,0 +1,28 @@
+let apples: number = 5;
+apples = 10;
+let speed: string = 'fast';
+let hasName: boolean = true;
+
+let nothingMuch: null = null;
+
+let now: Date = new Date();
+
+//Array
+
+let colors: string[] = ['red', 'green', 'blue'];
+let myNumbers: number[] = [1,2,3];
+let truths: boolean[] = [true, true, false];
+
+//classes
+class Car {
+
+}
+let car: Car = new Car();
+
+let point: {x: number ; y: number } = {
+    x: 10,
+    y: 15
+}
+const logNumber: (i:number) => void = (i: number) => {
+    console.log(i);
+}
