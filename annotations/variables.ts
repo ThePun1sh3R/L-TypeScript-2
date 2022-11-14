7316df679328b0f4465de9be5248771dd3795cb8
Amin Payandeh Far <amin.payandehfar94@gmail.com>
2022-11-14 19:57:51 +0330

    update variables.ts file

--- annotations/variables.ts
+++ annotations/variables.ts
@@ -4,6 +4,7 @@ let speed: string = 'fast';
 let hasName: boolean = true;
 
 let nothingMuch: null = null;
+let nothing: undefined = undefined;
 
 let now: Date = new Date();
 
@@ -25,6 +26,47 @@ let point: {x: number ; y: number } = {
 }
 const logNumber: (i:number) => void = (i: number) => {
     console.log(i);
-}
+};
 
 logNumber(10);
+
+//when to use annotations
+// 1) Function that returns the 'any' type
+
+const json = '{"x": 10, "y": 20}';
+const coordinates = JSON.parse(json);
+console.log(coordinates); // {x: 10, y: 20};
+
+// 2) when we declare a variable on one line 
+// and initializate it later
+
+let words = ['red', 'green', 'blue'];
+let foundWord: boolean;
+
+for (let i = 0; i < words.length; i++) {
+    if (words[i] === 'green') {
+        foundWord = true;
+    }
+}
+
+// 3) Variabl whose type cannot be inferred correctly
+
+let numbers = [-10 , -1 , 12];
+let numberAboveZero: boolean | number = false;
+
+for(let i = 0; i < numbers.length; i++) {
+    if (numbers[i] > 0) {
+        numberAboveZero = numbers[i];
+    } else {
+        numberAboveZero = false;
+    }
+}
+console.log(numberAboveZero);
+
+// funtions 
+const add = (a: number , b: number): number => {
+    return a + b;
+};
+
+console.log(add(10,30));
+
